f0ddf396ca7619ae5e10c3824803f148d6ad31cf
haldokan <haldokan@gmail.com>
2015-07-19 23:14:25 -0400

    Add code docs

--- GoEdge/src/org/haldokan/edge/rss-server/rss-server.go
+++ GoEdge/src/org/haldokan/edge/rss-server/rss-server.go
@@ -7,34 +7,55 @@ import (
  "strconv"
  "time"
 )
+/*
+This RSS server, based on a talk given by a Google engineer, shows advanced usage of concurrency in GO using "chan". It also
+demonstrate how structs implement interfaces.
+
+The RSS server fetches RSS updates from multiple RSS channels (dummy ones) concurrently and merges the updates into one update "chan".
+Each RSS channel fetcher implements a staggering policy for fetching updates where by it maintains a fixed size queue of pending updates.
+Fetching happens only when the queue size is less than the max size and the next fetching time is due.
+
+My next goal is to implement this server in Java in order to appreciate what having "chan" as part of the language contributes to 
+making hard-to-implement constructs (in Java) relatively easy to do in GO even for a beginner.
+*/
 
 // can also simply use rand.Seed(int64)
 var r *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
 
+// RSS update has id, title and the name of the RSS channel (not the GO "chan"; naming is unfortunate tho)
 type Item struct{ ID, Title, Chan string }
 
+// RSS fetcher returns a list of RSS updates, the time the next fetch is due, or an error if fetching fails.
+// This struct will be implemented by struct DataFetched further down
 type Fetcher interface {
  fetch() (items []Item, next time.Time, err error)
 }
 
+// RSS fetched batch has a list of items, the time the next fetch is due, and any errors.
+// This struct will implement the Fetcher interface further down
 type DataFetched struct {
  items []Item
  next  time.Time
  err   error
 }
 
+// RSS channel (feed would be a better name) subscriber interface has a func that returns a "chan" of update items,
+// a func to close the subscriber, and a hard-to-understand chan of chan of error: basically we are passing a chan to a chan
+// I will try to explain that later. This interface will be implemented by by struct Subscription further down
 type Subscriber interface {
  UpdatesStream() <-chan Item
  Close() error
  ClosingChan() chan chan error
 }
 
+// Subscription has a fetcher, an update chan of items and the curious chan of char of erro
 type Subscription struct {
  fetcher Fetcher
  updates chan Item
  closeit chan chan error
 }
 
+// The struct Subscription implementation of the Subscriber interface: the next 3 funcs 
 func (s *Subscription) UpdatesStream() <-chan Item {
  return s.updates
 }
@@ -50,15 +71,23 @@ func (s *Subscription) Close() error {
  return <-ch
 }
 
+// func Subscribe takes a fetcher and returns a Subscription (which implements Subscriber as we saw earlier)
 func Subscribe(fetcher Fetcher) Subscriber {
+ // here we create a pointer to Subscription by assigning the passed fetcher and making an update and an error chan
  s := &Subscription{
   fetcher: fetcher, updates: make(chan Item), closeit: make(chan chan error),
  }
+ // start a go routine to serve data using the Subscription fetcher and populating the the fetched data in the struct
+ // now how we simply indicate that a func "serve" has a target that is struct Subscription (have a look at the "serve" func)
  go s.serve()
  return s
 }
 
-//going to assume 3 subscriptions to avoid using reflection (func Select(cases []SelectCase) (chosen int, recv Value, recvOK bool))
+// The merger function takes a list of SubscriptionS and merge their updates into one outgoing updates chan. It also accepts
+// close whereupon it closes all the merged subscriptions and itself finally. Here the chan of chan we talked about comes to play.
+// Look at "case echan" (error chan). What this case receives is actually a chan. This enables each subscriber to send its close
+// request on its own chan (TODO: provide further details)
+// Going to assume 3 subscriptions to avoid using reflection (func Select(cases []SelectCase) (chosen int, recv Value, recvOK bool))
 func Merge(subs ...Subscriber) Subscriber {
  s := &Subscription{updates: make(chan Item), closeit: make(chan chan error)}
  go func() {
@@ -86,11 +115,14 @@ func Merge(subs ...Subscriber) Subscriber {
  return s
 }
 
+// struct DataFetched implements the Fetcher interface. It unpacks the fetched data and returns it
 func (d *DataFetched) fetch() (items []Item, next time.Time, err error) {
  d.next = time.Now().Add(time.Duration(2 * time.Second))
  return d.items, d.next, d.err
 }
 
+// A Generator function that returns dummy FetcherS. It takes an RSS channel (or feed) and randomly generate updates or errors.
+// As you might expect from your RSS feed it returns Buddhist wisdom in French!
 func Fetch(channel string) Fetcher {
  var (
   items []Item
@@ -105,15 +137,16 @@ func Fetch(channel string) Fetcher {
    {"item" + strconv.Itoa(rand.Intn(50)), "Je rend hommage a la compassion qui embrasse tous les etres.", channel}}
 
  }
+ // note how we return a pointer to DataFetched because it implements Fetcher (the return type of this func)
  return &DataFetched{items: items, err: err}
-
 }
 
-// serve fetches items using s.fetcher and sends them
-// on s.updates.  serve exits when s.Close is called.
+// Remember that the Subscribe func call this func in a goroutine. The func does the actual fetching of updates and channeling them
+// back to the calling funcs. There are a few cool construct here that are hard to implement in other languages (Java for instance)
 func (s *Subscription) serve() {
  serviceId := r.Int63()
  fmt.Println("starting service", serviceId)
+ // stagger fetching so no more that maxPending is queued
  const maxPending = 5
  var (
   next          time.Time
@@ -122,23 +155,27 @@ func (s *Subscription) serve() {
   firstItem     Item
   fetchingBatch chan *DataFetched
  )
+ // loop until told to close
  for {
-  //we insure not exceeding maxPending by having a nil channel that blocks
+  //we insure not exceeding maxPending by having a nil chan that blocks
   var nextFetch <-chan time.Time
 
+  // we want to fetch only if the current fetch has done and the pending updates less that max
   if fetchingBatch == nil && len(pendingData) < maxPending {
-   // create a chan only if the pending queue is ok
    nextFetch = time.After(next.Sub(time.Now()))
   }
+  // note how the "updates" chan is defined only if there are pedning data. The data is cosumed one item 
+  // at a time from the feching chan
   var updates chan Item
   if len(pendingData) > 0 {
    updates = s.updates
+   // at every loop read one item from the pending data
    firstItem = pendingData[0]
   }
   select {
   case <-nextFetch:
-   //we don't want to block on fetch so the pending queue keeps on draining and close signals are received. But at the same time we don't want to issue a fetch until the older
-   // fetch has finished
+   //we don't want to block on fetch so the pending queue keeps on draining and close signals are received. 
+   // But at the same time we don't want to issue a fetch until the older fetch has finished
    fetchingBatch = make(chan *DataFetched)
    go func() {
     var (
@@ -147,22 +184,27 @@ func (s *Subscription) serve() {
     )
     fmt.Println("Herr Bismark wird abrufen @", next)
     items, next, err = s.fetcher.fetch()
+    // send the fetched data on the chan fetchingBach which will be selected further down
     fetchingBatch <- &DataFetched{items, next, err}
    }()
 
-  //nil channels block
+  //send the first item in the pending data read above on the updates chan. Nil channels block
   case updates <- firstItem:
+   // slice the pending data so the first item is gone
    pendingData = pendingData[1:]
   case echan := <-s.closeit:
    echan <- fmt.Errorf("%s: %d", "breaking the service loop for service", serviceId)
    close(s.updates)
    return
+  // the fetched batch that was send on the chan is consume here
   case df := <-fetchingBatch:
    fetchingBatch = nil
    if df.err != nil {
-    fmt.Println("Error: Shit happens!")
+    fmt.Println("Error: Crap happens!")
     next = time.Now().Add(10 * time.Second)
    } else {
+    // read the items one at a time from the fetched data and put it in the pendingData list
+    // we filter out duplicate updates
     for _, i := range df.items {
      _, ok := dupFilter[i.ID]
      if !ok {
@@ -189,7 +231,7 @@ func main() {
  time.AfterFunc(10*time.Second, func() {
   fmt.Println("closed:", merged.Close())
  })
- // Print the stream.
+ // Here's how we print the data that become available on the updates stream
  for it := range merged.UpdatesStream() {
   fmt.Println("Received... ", it.ID, it.Chan, it.Title)
  }
